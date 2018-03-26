1bd2ad45967a406977eefd1396bc3f743edaa2af
indahutami <37090370+indahutami@users.noreply.github.com>
2018-03-26 21:56:37 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PrakiraanCuaca from './src/comp/PrakiraanCuaca';
+
+export default class App extends React.Component {
+  render() {
+    return (
+    <PrakiraanCuaca />
+    );
+  }
+}
